6041a2abd379e55ae0af3c10a3bbbceb1f503bf7
chrispsheehan <chrispsheehan@gmail.com>
2024-07-02 21:07:01 +0100

    $default api path

--- tf/main.tf
+++ tf/main.tf
@@ -74,3 +74,10 @@ resource "aws_apigatewayv2_route" "this" {
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.this.id}"
 }
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+}
+
