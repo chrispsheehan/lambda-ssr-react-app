4c198b434587cbbc9e8b2a0d1376a2a061515da4
chrispsheehan <chrispsheehan@gmail.com>
2024-09-02 15:26:21 +0100

    docker support

--- tests/favicon.spec.ts
+++ tests/favicon.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-const stage = process.env.STAGE;
-
 test('favicon is loading', async ({ page }) => {
-    const response = await page.goto(`${stage}/public/assets/favicon.ico`);
+    const response = await page.goto(`/public/assets/favicon.ico`);
     expect(response).not.toBeNull();
     expect(response?.status()).toBe(200);
 });
