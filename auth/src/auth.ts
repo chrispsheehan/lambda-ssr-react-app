03a39f5a835a7dd2306777c0e381b0fddf47a416
chrispsheehan <chrispsheehan@gmail.com>
2024-09-04 19:57:28 +0100

    fix policy

--- auth/src/auth.ts
+++ auth/src/auth.ts
@@ -4,22 +4,22 @@ import { APIGatewayAuthorizerResult, PolicyDocument, StatementEffect, APIGateway
  * A simple token-based authorizer function for AWS API Gateway.
  */
 export const handler = async (event: APIGatewayRequestAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
-    const token = event.headers?.Authorization || event.headers?.authorization;;
-    const expectedToken = process.env.API_KEY ;
-    const cloudfrontArn = process.env.CLOUDFRONT_ARN || ""
+    const token = event.headers?.Authorization || event.headers?.authorization;
+    const expectedToken = process.env.API_KEY;
+    const cloudfrontArn = process.env.CLOUDFRONT_ARN || "";
 
     // Validate the incoming token
     if (token === expectedToken) {
-        return generatePolicy('user', 'Allow', event.methodArn, cloudfrontArn);
+        return generatePolicy('user', 'Allow', event.methodArn);
     } else {
-        return generatePolicy('user', 'Deny', event.methodArn, cloudfrontArn);
+        return generatePolicy('user', 'Deny', event.methodArn);
     }
 };
 
 /**
  * Generates a policy document to allow or deny access to the API Gateway.
  */
-const generatePolicy = (principalId: string, effect: StatementEffect, resource: string, cloudfrontArn: string): APIGatewayAuthorizerResult => {
+const generatePolicy = (principalId: string, effect: StatementEffect, resource: string): APIGatewayAuthorizerResult => {
     const policyDocument: PolicyDocument = {
         Version: '2012-10-17',
         Statement: [
@@ -27,14 +27,6 @@ const generatePolicy = (principalId: string, effect: StatementEffect, resource: 
                 Effect: effect,
                 Action: 'execute-api:Invoke',
                 Resource: resource,
-                Principal: {
-                    Service: 'cloudfront.amazonaws.com',
-                },
-                Condition: {
-                    StringEquals: {
-                        'AWS:SourceArn': cloudfrontArn,
-                    },
-                },
             },
         ],
     };
@@ -42,7 +34,7 @@ const generatePolicy = (principalId: string, effect: StatementEffect, resource: 
     console.log({
         principalId,
         policyDocument,
-    })
+    });
 
     return {
         principalId,
